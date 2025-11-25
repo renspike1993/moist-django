c7631e20ba17eb7fb413cee97aad6ea403405a2c
renspike1993 <cold.renspike@gmail.com>
2025-11-25 21:52:30 +0800

    asd

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,18 +115,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # -------------------------------------------------------------------
 # Database (SQLite default)
 # -------------------------------------------------------------------
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# import dj_database_url
+
+# DATABASES = {
+#     'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+# }
 # -------------------------------------------------------------------
 # Authentication Redirects
 # -------------------------------------------------------------------
